a4eb0ed0c590254656fed27fddf8b94a2fcc23fb
maidenhairTree <giantMaidenhairTree@gmail.com>
2016-06-12 01:33:59 +0900

    페이스북 프로필 사진 연동 확인

--- GoldSpoon_Tycoon/AppDelegate.swift
+++ GoldSpoon_Tycoon/AppDelegate.swift
@@ -7,16 +7,23 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        return true
+        
+        return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool
+    {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
     }
 
     func applicationWillResignActive(application: UIApplication) {
--- GoldSpoon_Tycoon/UserInfoViewController.swift
+++ GoldSpoon_Tycoon/UserInfoViewController.swift
@@ -7,29 +7,67 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
 
-class UserInfoViewController: UIViewController {
+class UserInfoViewController: UIViewController, FBSDKLoginButtonDelegate {
 
+    @IBOutlet var userProfileImage: UIImageView!
+    @IBOutlet var facebookLoginButton: FBSDKLoginButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        facebookLoginButton.delegate = self
+        
+        if FBSDKAccessToken.currentAccessToken() != nil{
+            print("current token exist!")
+            fetchProfile()
+        }
 
         // Do any additional setup after loading the view.
     }
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!)
+    {
+        facebookLoginButton.readPermissions = ["public_profile", "email", "user_friends"];
+        fetchProfile()
+    }
+    
+    func loginButtonWillLogin(loginButton: FBSDKLoginButton!) -> Bool {
+        return true
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!)
+    {
+        let loginManager: FBSDKLoginManager = FBSDKLoginManager()
+        loginManager.logOut()
+        
+        userProfileImage.image = UIImage(named: "Unknown_user")
+
+    }
+    
+    func fetchProfile() {
+        let parameters = ["fields" : "email, first_name, last_name, picture.type(large)"]
+        FBSDKGraphRequest(graphPath: "me", parameters: parameters).startWithCompletionHandler({ (connection, user, requestError) -> Void in
+            
+            if requestError != nil {
+                print(requestError)
+                return
+            }
+            
+            let userProfilePictureURL: String = (user.objectForKey("picture")?.objectForKey("data")?.objectForKey("url") as? String)!
+            
+            self.userProfileImage.image = UIImage(data: NSData(contentsOfURL: NSURL(string: userProfilePictureURL)!)!)
+        
+        })
+        
+    }
+
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
